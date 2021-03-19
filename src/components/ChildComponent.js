ad8982979710feac5924711cd3116d38ae1a6077
abhishek pandir <me19b069@smail.iitm.ac.in>
2021-03-19 12:56:36 +0530

    2

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -3,6 +3,11 @@ import React from 'react'
 const ChildComponent = (props) => {
   return (
     <div>
+      {/* this is not a correct way to pass function  */}
+      {/* <button onClick={ props.greetHandler("child")}>Greet Parent</button> */}
+      {/* we can pass function directly without parameters  */}
+      <button onClick={ props.greetHandler}>Greet Parent</button>
+      {/* we can pass function as arrow function */}
       <button onClick={() => props.greetHandler('child')}>Greet Parent</button>
     </div>
   )
--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -5,43 +5,37 @@ class UserGreeting extends Component {
     super(props)
 
     this.state = {
-      isLoggedIn: true
+      isLoggedIn: false
     }
   }
 
-  // #if-else approach
-  // render() {
-  //   if (this.state.isLoggedIn) {
-  //     return <div>Welcome Vishwas</div>
-  //   } else {
-  //     return <div>Welcome Guest</div>
-  //   }
-  // }
+  render(){
+    // if (this.state.isLoggedIn) {
+    //   return (
+    //     <div>
+    //       Welcome Vishwas
+    //     </div>
+    //   )
+    // }else{
+    // return (
+    //   <div>
+    //     Welcome guest
+    //   </div>
+    // )
+    // let message
+    // if (this.state.isLoggedIn) {
+    //   message=<div>welcome Viswas </div>
+    // }else{
+    //   message=<div>welcome guest </div>
+    // }
 
-  // #element-variables approach
-  // render() {
-  //   let message
-  //   if (this.state.isLoggedIn) {
-  //     message = <div>Welcome Vishwas</div>
-  //   } else {
-  //     message = <div>Welcome Guest</div>
-  //   }
-  //   return <div>{message}</div>
-  // }
-
-  // #ternary-operator-approach
-  // render() {
-  //   return this.state.isLoggedIn ? (
-  //     <div>Welcome Vishwas</div>
-  //   ) : (
-  //     <div>Welcome Guest</div>
-  //   )
-  // }
-
-  // #short-circuit-operator-approach
-  render() {
-    return this.state.isLoggedIn && <div>Welcome Vishwas</div>
+    return(
+     this.state.isLoggedIn?
+     <div> Welcome viswas</div>:<div>welcome Guest</div>
+      
+    )
+    }
   }
-}
+
 
 export default UserGreeting
